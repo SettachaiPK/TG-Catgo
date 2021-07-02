8fd4129b205acda77ffbf610bc7cbc9cec558cc5
Nattakrit Jatupattaradit <nattakritj@outlook.com>
2021-07-03 00:44:13 +0700

    Fix job model

--- app/controllers/job.controller.js
+++ app/controllers/job.controller.js
@@ -71,7 +71,6 @@ exports.overviewAllJob = (req, res) => {
                     {"flightNumber":{ "$regex": req.query.search, "$options": "i" }},
                     {"hwbSerialNumber":{ "$regex": req.query.search, "$options": "i" }},
                     {"customsEntryNumber":{ "$regex": req.query.search, "$options": "i" }},
-                    {"numberOfPieces":{ "$regex": req.query.search, "$options": "i" }},
                     {"dockNumber":{ "$regex": req.query.search, "$options": "i" }},
                     {"truckNumber":{ "$regex": req.query.search, "$options": "i" }},
                     {"customsEntryNumberDate":{ $gt: date_input, $lt: date_input_24hr }},
@@ -112,6 +111,7 @@ exports.createJob = async (req, res) => {
             jobNumber: req.body.jobNumber,
             customsEntryNumber: req.body.customsEntryNumber,
             customsEntryNumberDate: req.body.customsEntryNumberDate,
+            numberOfPieces: 555,
         });
 	job.flightDate = Date.now();
         await job.save()
